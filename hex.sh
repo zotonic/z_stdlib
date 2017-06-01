0062edd324eafc463d37f3287565d2691641e9cf
David de Boer <david@ddeboer.nl>
2017-06-01 14:55:44 +0200

    Fix Travis deploy

--- hex.sh
+++ hex.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+mkdir -p ~/.hex
 echo '{username,<<"'${HEX_USERNAME}'">>}.' > ~/.hex/hex.config
 echo '{key,<<"'${HEX_KEY}'">>}.' >> ~/.hex/hex.config
 
-rebar3 hex publish <<EOF
+mkdir -p ~/.config/rebar3
+echo '{plugins, [rebar3_hex]}.' > ~/.config/rebar3/rebar.config
+
+./rebar3 hex publish <<EOF
 y
 EOF
