02fbfa4c134b1302300af0ce7113d156b09539eb
Marc Worrell <marc@worrell.nl>
2018-03-14 13:57:58 +0100

    Add jsmin (#33)
    (cherry picked from commit 200487a1d90dbef0d0543aba2d9d4da79e49012c)

--- README.md
+++ README.md
@@ -1,4 +1,118 @@
+
 z_stdlib
 ========
 
+[![Build Status](https://travis-ci.org/zotonic/z_stdlib.svg?branch=master)](https://travis-ci.org/zotonic/z_stdlib)
+
 Zotonic standard library - a library with hopefully useful functions
+
+Extracted from Zotonic, the Erlang CMS http://zotonic.com/
+
+
+Installation
+------------
+
+You can clone it directly:
+
+    git clone https://github.com/zotonic/z_stdlib.git
+
+Or add as a dependency in rebar:
+
+    {deps, [
+        {z_stdlib, ".*", {git, "https://github.com/zotonic/z_stdlib.git", "0.x"}}
+    ]}.
+
+
+Modules
+-------
+
+The following modules are provided:
+
+
+### z_convert
+
+Conversion routines for:
+
+ * all common data types (binary, boolean, integer, etc.).
+ * dates: to_utc, to_local, iso8601 parsing
+ * json, convert to simple json constructs
+ * IP address conversions
+
+
+### z_css
+
+Strict CSS parser and sanitizer.
+
+
+### z_dateformat
+
+Routines for formatting dates.
+Example:
+
+    z_dateformat:format({{2008,12,10},{15,30,0}}, "Y-m-d H:i:s", [ {tz, "GMT"} ]).
+
+
+### z_email_dnsbl
+
+Check IP addresses for their presence on DNS black- or whitelists.
+
+
+### z_email_utils
+
+Test if an email address is syntactically valid. Extract email addresss from a text.
+
+
+### z_filelib
+
+Convenience function to make nested directories.
+
+
+### z_html
+
+HTML routines like escape, unescape, sanitize, etc.
+
+
+### z_ip_address
+
+Match an IP address against lists, check if an IP address is a non routable LAN address.
+
+
+### z_jsmin
+
+JavaScript minimizer, removes spaces, comments and newlines.
+
+
+### z_string
+
+String routines. To uppercase, lowercase, truncate, trim, utf8 checks, and more.
+
+
+### z_svg
+
+SVG sanitizer.
+
+
+### z_tempfile
+
+Temporary file routines. Make tempfiles, including watch dog process to clean up the file.
+
+
+### z_ubf
+
+UBF-A encoding and decoding routines. Safe for atoms and maximum memory size.
+
+
+### z_url
+
+URL encode, decode and more.
+
+
+### z_url_fetch
+
+Fetch the first N bytes of an URL. Protection against too large return body.
+
+
+### z_url_metadata
+
+Fetch an URL, extract metadata like mime type, title, description, images etc.
+
---
+++ test/z_jsmin_test.erl
@@ -0,0 +1,108 @@
+%% @author Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
+
+-module(z_jsmin_test).
+
+-include_lib("eunit/include/eunit.hrl").
+
+% Tests from: https://github.com/tikitu/jsmin/blob/master/jsmin/test.py
+
+
+remove_comment_and_whitespace_test() ->
+    ?assertEqual(<<>>, minify(<<>>)),
+    ?assertEqual(<<>>, minify(<<"    ">>)),
+    ?assertEqual(<<>>, minify(<<"\n   ">>)),
+    ?assertEqual(<<>>, minify(<<"\r\n">>)),
+    ?assertEqual(<<>>, minify(<<"\r\n\v\f\b">>)),
+    ?assertEqual(<<>>, minify(<<"// empty">>)),
+    ?assertEqual(<<>>, minify(<<"// one\n// two\n">>)),
+    ?assertEqual(<<>>, minify(<<"/* Just comment */">>)),
+    ?assertEqual(<<>>, minify(<<"/* Multi line\n * one \n * two \n*/">>)),
+    ?assertEqual(<<>>, minify(<<"/* Multi line\n * one \n * two \n*/\n", 
+                                "// line comment">>)),
+    ok.
+
+simple_expressions_test() ->
+    ?assertEqual(<<"s">>, minify(<<"s">>)),
+    ?assertEqual(<<"1+2">>, minify(<<"1 + 2">>)),
+    ?assertEqual(<<"1+2;">>, minify(<<"1 /* one */ + 2; // two">>)),
+    ok.
+
+implicit_semicolon_test() ->
+    ?assertEqual(<<"return\na">>, minify(<<"return\na">>)).
+
+explicit_semicolon_test() ->
+    ?assertEqual(<<"return;a">>, minify(<<"return;//comment\r\na">>)).
+
+space_in_regexp_test() ->
+    ?assertEqual(<<"/a (a)/.test(\"a\")">>, <<"/a (a)/.test(\"a\")">>).
+
+newline_between_strings_test() ->
+    ?assertEqual(<<"\"yolo\"\n\"yolo\"">>, <<"\"yolo\"\n\"yolo\"">>).
+
+comments_between_tokens_test() ->
+    ?assertEqual(<<"var a">>, minify(<<"var /* comment */ a">>)).
+
+ends_with_string_test() ->
+    ?assertEqual(<<"var s=\"s\"">>, minify(<<"var s = \"s\"">>)).
+
+short_comment_test() ->
+    ?assertEqual(<<"a;b">>, minify(<<"a;/**/b">>)).
+
+shorter_comment_test() ->
+    ?assertEqual(<<"a;b">>, minify(<<"a;/*/*/b">>)).
+
+block_comment_with_semicolon_test() ->
+    ?assertEqual(<<"a;b">>, minify(<<"a;/**/\nb">>)).
+    
+block_comment_with_implicit_semicolon_test() ->
+    ?assertEqual(<<"a\nvar b">>, minify(<<"a/**/\nvar b">>)).
+
+single_comments_test() ->
+    Lines = ["",
+             "var a = \"hello\" // this is a comment",
+             "    a += \" world\"",
+             ""],
+    
+    ?assertEqual(<<"var a=\"hello\"\na+=\" world\"">>, minify_multi_line(Lines)).
+
+
+multi_comments_test() ->
+    Lines = ["",
+             "var a = \"hello\" /* this is a comment */",
+             "    a += \" world\"",
+             ""],
+
+    ?assertEqual(<<"var a=\"hello\"\na+=\" world\"">>, minify_multi_line(Lines)).
+
+re_nl_if_test() ->
+    Lines = ["",
+             "var re = /\\d{4}/",
+             "if (1) { console.log(2); }"],
+    ?assertEqual(<<"var re=/\\d{4}/\nif(1){console.log(2);}">>, minify_multi_line(Lines)). 
+
+
+copyright_comment_test() ->
+    ?assertEqual(<<"/*! hello */\na;b">>, minify(<<"/*! hello */\na;\nb">>)).
+
+
+char_from_return_test() ->
+    ?assertEqual(<<"return r/1;">>, minify(<<"return r / 1;">>)).
+    
+precedence_of_parens_test() ->
+    ?assertEqual(<<"/a([)])b/g">>, <<"/a([)])b/g">>),
+    ?assertEqual(<<"/a[(]b/g">>, <<"/a[(]b/g">>),
+    ok.
+
+
+%%
+%% Helpers
+%%
+
+minify_multi_line(Lines) when is_list(Lines) -> 
+    minify(list_to_binary(string:join(Lines, "\n"))).
+
+minify(Bin) when is_binary(Bin) ->
+    z_jsmin:minify(Bin).
+
+    
+
