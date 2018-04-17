1980e96c52dd53ad640d830b2552725990d3ac20
Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
2018-04-17 11:07:23 +0200

    Wip cssmin (#35)
    * Added css minifier based on cssmin.c
    
    * Skip spaces in selector
    
    * Reduce zero units
    
    * Condense multiple semicolons

---
+++ test/z_cssmin_test.erl
@@ -0,0 +1,41 @@
+%% @author Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
+
+-module(z_cssmin_test).
+
+-include_lib("eunit/include/eunit.hrl").
+
+simple_test() ->
+    ?assertEqual(<<>>, minify(<<>>)),
+    ?assertEqual(<<>>, minify(<<"    ">>)),
+    ?assertEqual(<<>>, minify(<<"/* comment */">>)),
+    ok.
+
+class_selector_test() ->
+    ?assertEqual(<<"p :link">>, minify(<<"p :link">>)),
+
+    ?assertEqual(<<"code.html{color: #191970}">>, minify(<<"code.html { color:    #191970; }">>)),
+    ?assertEqual(<<"code.html{color: #191970}">>, minify(<<"code.html     { color:    #191970; }">>)),
+
+    %% The comment after the selector causes the insertion of an extra space
+    %% it also leaves an extra colon after the last element of a block
+    ?assertEqual(<<"code.html { color: #191970; }">>, 
+                 minify(<<"/* a */code.html  /* b */  { /* c */ color: /* d */  #191970; /* e */  } /* f */">>)),
+    ok.
+
+condense_multiple_semicolons_test() ->
+    ?assertEqual(<<".btn{color: red;display: block}">>, minify(<<".btn { color: red;; display: block;;;}">>)),
+    ok.
+
+minimize_zero_units_test() ->
+    ?assertEqual(<<"div{border: 10px}">>, minify(<<"div {border: 10px}">>)),
+    ?assertEqual(<<"div{border: 0px}">>, minify(<<"div {border:   0px}">>)),
+    ?assertEqual(<<"div{border: 0pt}">>, minify(<<"div {border:   0pt}">>)),
+    ?assertEqual(<<"div{width: 0%;border: 0in}">>, minify(<<"div {width: 0%; border: 0in}">>)),
+    ok.
+
+%%
+%% Helpers
+%%
+
+minify(Bin) ->
+    z_cssmin:minify(Bin).
