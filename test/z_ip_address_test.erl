8b226feb0f9725f9ab9c0c09c3fbeb2055cf3113
Marc Worrell <marc@worrell.nl>
2022-09-23 11:21:29 +0200

    Use assertEqual in test

--- test/z_ip_address_test.erl
+++ test/z_ip_address_test.erl
@@ -5,10 +5,10 @@
 -include_lib("eunit/include/eunit.hrl").
 
 is_local_test() ->
-    true = z_ip_address:is_local_name(<<"localhost">>),
-    true = z_ip_address:is_local_name("localhost"),
-    true = z_ip_address:is_local_name("127.0.0.1"),
-    true = z_ip_address:is_local_name("::1"),
-    true = z_ip_address:is_local_name("192.168.1.10"),
-    true = z_ip_address:is_local_name(<<"192.168.1.10">>),
-    false =  z_ip_address:is_local_name(<<"8.8.8.8">>).
+    ?assertEqual(true, z_ip_address:is_local_name(<<"localhost">>)),
+    ?assertEqual(true, z_ip_address:is_local_name("localhost")),
+    ?assertEqual(true, z_ip_address:is_local_name("127.0.0.1")),
+    ?assertEqual(true, z_ip_address:is_local_name("::1")),
+    ?assertEqual(true, z_ip_address:is_local_name("192.168.1.10")),
+    ?assertEqual(true, z_ip_address:is_local_name(<<"192.168.1.10">>)),
+    ?assertEqual(false, z_ip_address:is_local_name(<<"8.8.8.8">>)).
